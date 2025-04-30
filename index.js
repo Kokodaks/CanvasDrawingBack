574afbd36455813bb19a3d3466e5432c8e0c4f88
Kokodaks <dayeonko357@gmail.com>
2025-04-30 13:27:21 +0900

    added : reconstruction mongodb collection

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const mysqlDB = require('./database/mysql/models');
-// const connectMongoDB = require('./database/mongodb/config/mongodbConfig');
-// const syncMongoDB = require('./database/mongodb/models/index.js');
+// const mysqlDB = require('./database/mysql/models');
+const connectMongoDB = require('./database/mongodb/mongdbConfig.js');
+const syncMongoDB = require('./database/mongodb/models/index.js');
 
 //mysql 스키마들을 한번에 담은 db 객체를 sequelize.sync()해서 실제 mysql db에 적용
-(async () => {
-    await mysqlDB.sequelize.sync({force: true});
-})();
+// (async () => {
+//     await mysqlDB.sequelize.sync({force: true});
+// })();
 
-// const mongoDB = async() => {
-//     await connectMongoDB();
+const mongoDB = async() => {
+    await connectMongoDB();
     
-//     //mongodb models의 index.js 모듈 실행
-//     await syncMongoDB();
-// };
+    //mongodb models의 index.js 모듈 실행
+    await syncMongoDB();
+};
 
-// mongoDB();
+mongoDB();
