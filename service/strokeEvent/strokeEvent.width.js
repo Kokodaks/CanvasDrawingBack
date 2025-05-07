22827d87c4c5abcfba28ea1e34650fe2bed86bd6
Kokodaks <dayeonko357@gmail.com>
2025-05-08 04:40:43 +0900

    updated : drawingStrokes model

--- service/strokeEvent/strokeEvent.width.js
+++ service/strokeEvent/strokeEvent.width.js
@@ -1,11 +1,20 @@
 exports.findWidthEvent = (finalDrawing, DrawingStrokesId) => {
-    const widths = finalDrawing.map(stroke => stroke.strokeWidth);
+    const widths = finalDrawing.flatMap(stroke => stroke.points.map(point => point.p));
 
     const avgWidth = widths.reduce((sum, w) => sum + w, 0) / widths.length;
     const epsilon = 0.1 * avgWidth;
 
-    const thickStrokes = finalDrawing.filter(stroke => stroke.strokeWidth > avgWidth + epsilon);
-    const thinStrokes = finalDrawing.filter(stroke => stroke.strokeWidth < avgWidth - epsilon);
+    const thickStrokes = finalDrawing.filter(stroke => {
+        const pressures = stroke.points.map(p => p.p);
+        const avgP = pressures.reduce((sum, p) => sum + p, 0) / pressures.length;
+        return avgP > avgWidth + epsilon;
+    });
+
+    const thinStrokes = finalDrawing.filter(stroke => {
+        const pressures = stroke.points.map(p => p.p);
+        const avgP = pressures.reduce((sum, p) => sum + p, 0) / pressures.length;
+        return avgP < avgWidth + epsilon;
+    });
     
     const thickEvents = convertToThickEvent(thickStrokes, DrawingStrokesId);
     const thinEvents = convertToThinEvent(thinStrokes, DrawingStrokesId);
