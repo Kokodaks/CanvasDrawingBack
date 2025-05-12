06578e93d74f0b51079b1b36da9a0b7de382f84d
Kokodaks <dayeonko357@gmail.com>
2025-05-13 00:44:02 +0900

    🔁 재배포 테스트. fixed : openAI API error

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const port = 3000;
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const path = require('path');
-// require('dotenv').config();
 const dotenv = require('dotenv');
+const path = require('path');
+require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
